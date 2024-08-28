ca6c61d2ce230aa736f801e3463e1dd2df67dbe3
James Liu <51351043+chromecast56@users.noreply.github.com>
2024-08-28 15:00:34 -0700

    Update ppl_test.bash

--- scripts/ppl_test.bash
+++ scripts/ppl_test.bash
@@ -1,5 +1,5 @@
 # TEAL_PATH is the output path specified in grab_acts.py
 
-CUDA_VISIBLE_DEVICES=0 python teal/ppl_test.py --model_name meta-llama/Llama-2-7b-hf --teal_path /data_persistent2/jamesliu/sparsedoping/Llama-2-7B --sparsity 0.5
+CUDA_VISIBLE_DEVICES=0 python teal/ppl_test.py --model_name meta-llama/Llama-2-7b-hf --teal_path $OUTPUT_PATH --sparsity 0.5
 
 
