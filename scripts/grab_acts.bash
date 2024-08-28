4e6e3bd92aebcd4115b6dcf881c6c9f633bdc9ad
James Liu <51351043+chromecast56@users.noreply.github.com>
2024-08-28 15:00:07 -0700

    Update grab_acts.bash

--- scripts/grab_acts.bash
+++ scripts/grab_acts.bash
@@ -1,4 +1,4 @@
 # Specify output path to store activations and histograms
 
 
-CUDA_VISIBLE_DEVICES=0 python teal/grab_acts.py --model_name meta-llama/Llama-2-7b-hf --output_path /data_persistent2/jamesliu/teal/Llama-2-7B
+CUDA_VISIBLE_DEVICES=0 python teal/grab_acts.py --model_name meta-llama/Llama-2-7b-hf --output_path $OUTPUT_PATH
